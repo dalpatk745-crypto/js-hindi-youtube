cfadbd3412f6c51aab0526eb7de8323837ca1578
dalpatk745-crypto <dalpatk745@gmail.com>
2026-06-13 07:40:31 +0000

    data types and variables declaration learn

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId=144522
+let accountEmail="dalpat@gmail.com"
+var accountPassword="12345"
+accountCity="jodhpur"
+
+
+
+accountEmail="dalpa@gmail.com"
+accountPassword="3226565"
+accountCity="ahmadabad"
+/*
+prefer not to use var bacause of issue in block scope and functional scope
+*/
+
+console.log(accountId);
+console.table([accountId,accountPassword,accountCity,accountEmail])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3) // we are using nodejs ,not browser
+
+console.log(3
+    +3   //code readability should be high
+)
+
+
+let name="dalpat"
+let age=21
+let isLoggedin=false
+
+// number=> 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefinded => no value asign till now
+// symbol=> unique
+// object
+
+console.log(typeof("dalpat"))
