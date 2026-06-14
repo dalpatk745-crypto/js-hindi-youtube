77f40dd3a37c86e369302bf68697d32996053478
dalpatk745-crypto <dalpatk745@gmail.com>
2026-06-14 18:33:52 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score=33
+console.log(typeof(score))
+
+let name="dalpat"
+let age="21"
+console.log(typeof(name))
+console.log(typeof(age))
+
+let valueinNumber=Number(age)
+console.log(typeof(valueinNumber))
+
+
+// "33" =>33 
+// "33abc" => Nan
+// true => 1 ; false => 0
+
+let IsLoggedIn = 1
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+console.log(booleanIsLoggedIn) 
+
+// true => 1, false =>0
+// "" => false
+// "dalpat" => true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
