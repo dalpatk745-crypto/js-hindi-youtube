5b216191f7f0ba609ced55b093b8224878f4c49b
dalpatk745-crypto <dalpatk745@gmail.com>
2026-06-18 08:58:27 +0000

    welcome to strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,9 @@
+let name="dalpat"
+const age=21
+const salary=200000
+// string interpolation
+console.log(`dalpat is working in google at the age of ${age} and now her salary is ${salary} per months`)
+  
+const newstring = "  dalpat kumar  "
+console.log(newstring)
+console.log(newstring.trim())
